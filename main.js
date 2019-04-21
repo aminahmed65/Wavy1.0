6e0021181f3d129af2e31293af81ef38df54bea8
aminahmed65 <48972428+aminahmed65@users.noreply.github.com>
2019-04-21 13:44:08 -0400

    Importing Wavy 1.0

---
+++ main.js
@@ -0,0 +1,7 @@
+//implement a blur effect to the background on each sidebar
+//definetly a nav bar to a like for github and some water charity
+//paralaxing
+//angles in the site to add some more umph
+//  mix-blend-mode: lighten;
+//  border: 2px solid black;/*#ff9933 */
+// background-color: white;
